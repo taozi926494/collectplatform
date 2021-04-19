5be82d0b84778c5c1aee82867b561e1262c1c57e
Evan <bestfuqiang@163.com>
2021-04-19 16:26:11 +0800

    合并

--- project/src/main/java/com/collectplatform/project/ProjectApplication.java
+++ project/src/main/java/com/collectplatform/project/ProjectApplication.java
@@ -5,16 +5,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
+
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-=======
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
->>>>>>> 1dec0cd3c619c97f647bbf21fc0e564304835922
+
 
 
 
@@ -24,12 +20,8 @@ import org.springframework.context.annotation.ComponentScans;
 })
 @SpringBootApplication
 @MapperScan("com.collectplatform.project.dao")
-<<<<<<< HEAD
-//@EnableFeignClients(basePackages = {"com.collectplatform.core.common.feign"})
-//@EnableDiscoveryClient
-=======
+
 @ComponentScan(basePackages = {"com.collectplatform.project", "com.collectplatform.core.common"})
->>>>>>> 1dec0cd3c619c97f647bbf21fc0e564304835922
 public class ProjectApplication {
 
 	public static void main(String[] args) {
